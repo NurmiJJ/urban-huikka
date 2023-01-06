7d34bea35feec0e11b6c3b65aaf3485352357827
Matias Nurmi <matias.nurmi@tuni.fi>
2023-01-06 18:38:28 +0200

    Add base for the app

---
+++ app/src/main/java/fi/sabriina/urbanhuikka/StartActivity.kt
@@ -0,0 +1,11 @@
+package fi.sabriina.urbanhuikka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
