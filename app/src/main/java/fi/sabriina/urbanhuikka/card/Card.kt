70436e24287d9932d9b58af1db090944d50fa117
warrior25 <eetu.hakkinen@hotmail.com>
2023-07-12 19:34:53 +0300

    Support for new Firestore fields

--- app/src/main/java/fi/sabriina/urbanhuikka/card/Card.kt
+++ app/src/main/java/fi/sabriina/urbanhuikka/card/Card.kt
@@ -1,12 +1,8 @@
 package fi.sabriina.urbanhuikka.card
 
-const val LOREM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultricies mattis nulla eget fringilla. Integer facilisis leo nibh, sed viverra arcu condimentum nec. Pellentesque et consequat mi. In in mattis dui. Suspendisse elementum, ante sit amet fringilla rhoncus, nisl ante tempor mauris, sit amet sollicitudin magna nisl ornare lacus."
-
-
 data class Card(
-    val title: String = "",
-    val chapter: String = LOREM,
-    val addInfo: String = "",
-    val points: Int = 1
-
+    val category: String = "",
+    val description: String = "",
+    val points: Int = 0,
+    val time: Int = 0
 )
