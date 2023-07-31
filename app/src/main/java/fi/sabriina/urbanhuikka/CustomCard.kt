76c9a151d462b44fe3791eac4eea9b75714412ad
warrior25 <eetu.hakkinen@hotmail.com>
2023-07-31 20:34:55 +0300

    Improve card visuals and animations

--- app/src/main/java/fi/sabriina/urbanhuikka/CustomCard.kt
+++ app/src/main/java/fi/sabriina/urbanhuikka/CustomCard.kt
@@ -1,22 +1,18 @@
 package fi.sabriina.urbanhuikka
 
 import android.animation.Animator
-import android.animation.AnimatorInflater
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
+import android.view.animation.DecelerateInterpolator
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import fi.sabriina.urbanhuikka.card.Card
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 interface OnCardSwipeListener {
     fun onSwipeRight()
@@ -30,6 +26,9 @@ class CustomCard : ConstraintLayout {
     constructor(context: Context, attr: AttributeSet, defStyleAttr : Int) : super(context, attr, defStyleAttr )
 
     private var initialX = 0f
+    private var cardX = 0f
+    private var deltaX = 0f
+    private var isMoving = false
     private var swipeListener: OnCardSwipeListener? = null
 
     private var backside = true
@@ -58,13 +57,6 @@ class CustomCard : ConstraintLayout {
         backside = showBackside
         val frontView = findViewById<ConstraintLayout>(R.id.frontLayout)
         val backView = findViewById<ImageView>(R.id.cardBackside)
-
-        if (backside) {
-            Log.d(TAG, "backside")
-        }
-        else {
-            Log.d(TAG, "front")
-        }
         frontView.visibility = if (backside) View.GONE else View.VISIBLE
         backView.visibility = if (backside) View.VISIBLE else View.GONE
     }
@@ -83,25 +75,21 @@ class CustomCard : ConstraintLayout {
 
         val flipInAnimator : Animator
         val flipOutAnimator : Animator
-        val moveInAnimator : Animator
-        val moveOutAnimator : Animator
+
         if (direction == "left") {
-            flipOutAnimator = AnimatorInflater.loadAnimator(context, R.animator.card_flip_out_right)
-            flipInAnimator = AnimatorInflater.loadAnimator(context, R.animator.card_flip_in_right)
-            moveInAnimator = AnimatorInflater.loadAnimator(context, R.animator.card_move_in_left)
-            moveOutAnimator = AnimatorInflater.loadAnimator(context, R.animator.card_move_out_left)
+            flipOutAnimator = ObjectAnimator.ofFloat(this, "rotationY", 90f)
+            flipInAnimator = ObjectAnimator.ofFloat(this, "rotationY", -90f, 0f)
         }
         else {
-            flipOutAnimator = AnimatorInflater.loadAnimator(context, R.animator.card_flip_out_left)
-            flipInAnimator = AnimatorInflater.loadAnimator(context, R.animator.card_flip_in_left)
-            moveInAnimator = AnimatorInflater.loadAnimator(context, R.animator.card_move_in_right)
-            moveOutAnimator = AnimatorInflater.loadAnimator(context, R.animator.card_move_out_right)
+            flipOutAnimator = ObjectAnimator.ofFloat(this, "rotationY", -90f)
+            flipInAnimator = ObjectAnimator.ofFloat(this, "rotationY", 90f, 0f)
         }
 
-        flipOutAnimator.setTarget(backView)
-        flipInAnimator.setTarget(frontView)
-        moveOutAnimator.setTarget(backView)
-        moveInAnimator.setTarget(frontView)
+        flipOutAnimator.target = backView
+        flipInAnimator.target = frontView
+
+        val returnAnimator = ObjectAnimator.ofFloat(this, "translationX", 0f)
+        returnAnimator.interpolator = DecelerateInterpolator() // Add a decelerate effect
 
         frontView.cameraDistance = cameraDistance
         backView.cameraDistance = cameraDistance
@@ -111,7 +99,7 @@ class CustomCard : ConstraintLayout {
             override fun onAnimationEnd(animation: Animator) {
                 setCardSide(false)
                 flipInAnimator.start()
-                moveInAnimator.start()
+                returnAnimator.start()
             }
             override fun onAnimationCancel(animation: Animator) {}
             override fun onAnimationRepeat(animation: Animator) {}
@@ -121,55 +109,66 @@ class CustomCard : ConstraintLayout {
             override fun onAnimationStart(animation: Animator) {}
             override fun onAnimationEnd(animation: Animator) {
                 // reset backside to initial position
-                moveInAnimator.setTarget(backView)
-                flipInAnimator.setTarget(backView)
+                returnAnimator.target = backView
+                flipInAnimator.target = backView
 
                 // prevent infinite looping of animation
-                moveInAnimator.removeAllListeners()
+                returnAnimator.removeAllListeners()
                 flipInAnimator.removeAllListeners()
 
-                moveInAnimator.start()
+                returnAnimator.start()
                 flipInAnimator.start()
             }
             override fun onAnimationCancel(animation: Animator) {}
             override fun onAnimationRepeat(animation: Animator) {}
         })
 
-        moveOutAnimator.addListener(object : Animator.AnimatorListener {
-            override fun onAnimationStart(animation: Animator) {
-                CoroutineScope(Dispatchers.Main).launch {
-                    // start rotating shortly after card moves
-                    delay(50)
-                    flipOutAnimator.start()
-                }
-            }
-            override fun onAnimationEnd(animation: Animator) {}
-            override fun onAnimationCancel(animation: Animator) {}
-            override fun onAnimationRepeat(animation: Animator) {}
-        })
-
         // start first animation
-        moveOutAnimator.start()
+        flipOutAnimator.start()
     }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (backside) {
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    initialX = event.rawX
+                    cardX = x
 
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                initialX = event.x
-                return true
-            }
-            MotionEvent.ACTION_UP -> {
-                if (backside) {
-                    val deltaX = event.x - initialX
+                    // card was not moved
+                    deltaX = 0F
+                    isMoving = false
+
+                    return true
+                }
+
+                MotionEvent.ACTION_MOVE -> {
+                    // Calculate the distance moved by the finger
+                    deltaX = event.rawX - initialX
+
+                    // Update card position
+                    x = cardX + deltaX
+
+                    // Mark the card as moving to avoid conflicting with other touch events
+                    isMoving = true
+
+                    return true
+                }
+
+                MotionEvent.ACTION_UP -> {
                     if (deltaX > SWIPE_THRESHOLD) {
                         swipeListener?.onSwipeRight()
                         flipCard("right")
                     } else if (deltaX < -SWIPE_THRESHOLD) {
                         swipeListener?.onSwipeLeft()
                         flipCard("left")
+                    } else {
+                        // If the card was not moved enough, animate it back to the original position
+                        val returnAnimator = ObjectAnimator.ofFloat(this, "translationX", 0f)
+                        returnAnimator.interpolator = DecelerateInterpolator() // Add a decelerate effect
+                        returnAnimator.start()
                     }
+                    isMoving = false
                     return true
                 }
             }
@@ -178,6 +177,6 @@ class CustomCard : ConstraintLayout {
     }
 
     companion object {
-        private const val SWIPE_THRESHOLD = 100
+        private const val SWIPE_THRESHOLD = 300
     }
 }
