f7917280634c9b280c0d8eb6db8750227046f7ab
warrior25 <eetu.hakkinen@hotmail.com>
2023-08-16 21:53:10 +0300

    Add more interesting profile avatars

--- app/src/main/java/fi/sabriina/urbanhuikka/helpers/DbConstants.kt
+++ app/src/main/java/fi/sabriina/urbanhuikka/helpers/DbConstants.kt
@@ -1,6 +1,62 @@
 package fi.sabriina.urbanhuikka.helpers
 
+import fi.sabriina.urbanhuikka.R
+
 object DbConstants {
-    val DARE_CATEGORIES = listOf("Onks pakko?", "Ruoka ja juoma", "Luuri käteen", "Sinä ja minä", "Tieto ja taito")
-    val TRUTH_CATEGORIES = listOf("Nolot tilanteet ja kommellukset", "Haaveet ja unelmat", "Salaisuudet ja paljastukset", "Romantiikka ja ihmissuhteet", "Pelot ja epävarmuudet", "Seksi")
+    val DARE_CATEGORIES =
+        listOf("Onks pakko?", "Ruoka ja juoma", "Luuri käteen", "Sinä ja minä", "Tieto ja taito")
+    val TRUTH_CATEGORIES = listOf(
+        "Nolot tilanteet ja kommellukset",
+        "Haaveet ja unelmat",
+        "Salaisuudet ja paljastukset",
+        "Romantiikka ja ihmissuhteet",
+        "Pelot ja epävarmuudet",
+        "Seksi"
+    )
+    val AVATAR_ICONS = listOf(
+        R.drawable.multiavatar1,
+        R.drawable.multiavatar2,
+        R.drawable.multiavatar3,
+        R.drawable.multiavatar4,
+        R.drawable.multiavatar5,
+        R.drawable.multiavatar6,
+        R.drawable.multiavatar7,
+        R.drawable.multiavatar8,
+        R.drawable.multiavatar9,
+        R.drawable.multiavatar10,
+        R.drawable.multiavatar11,
+        R.drawable.multiavatar12,
+        R.drawable.multiavatar13,
+        R.drawable.multiavatar14,
+        R.drawable.multiavatar15,
+        R.drawable.multiavatar16,
+        R.drawable.multiavatar17,
+        R.drawable.multiavatar18,
+        R.drawable.multiavatar19,
+        R.drawable.multiavatar20,
+        R.drawable.multiavatar21,
+        R.drawable.multiavatar22,
+        R.drawable.multiavatar23,
+        R.drawable.multiavatar24,
+        R.drawable.multiavatar25,
+        R.drawable.multiavatar26,
+        R.drawable.multiavatar27,
+        R.drawable.multiavatar28,
+        R.drawable.multiavatar29,
+        R.drawable.multiavatar30,
+        R.drawable.multiavatar31,
+        R.drawable.multiavatar32,
+        R.drawable.multiavatar33,
+        R.drawable.multiavatar34,
+        R.drawable.multiavatar35,
+        R.drawable.multiavatar36,
+        R.drawable.multiavatar37,
+        R.drawable.multiavatar38,
+        R.drawable.multiavatar39,
+        R.drawable.multiavatar40,
+        R.drawable.multiavatar41,
+        R.drawable.multiavatar42,
+        R.drawable.multiavatar43,
+        R.drawable.multiavatar44,
+    )
 }
